38a5e81e5d42549feb5e68bb999aeabfc385054b
Adrian Marin <adru_95_arg@hotmail.com>
2021-05-28 19:21:11 -0300

    Agregadas las funciones basicas de add, update y delete

--- src/userFunctions.js
+++ src/userFunctions.js
@@ -1,20 +1,3 @@
-function printUserTable(data) {
-    let html = '';
-    for (let i = 0; i < data.length; i++) {
-        html += `<tr>\n\
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                    <button class='btn btn-sm btn-info pull-left check'>Editar</button>    
-                    <button class='btn btn-sm btn-info pull-left check'>Eliminar</button>
-                    </td>
-                </tr>`;
-    }
-    $("#user-table-body").html(html);
-};
-
 ipcRenderer.on('fill-user-table', (event, status) => {
     if (status.success) {
         printUserTable(status.data);
@@ -23,15 +6,126 @@ ipcRenderer.on('fill-user-table', (event, status) => {
     }
 });
 
-function checkUser() {
-    const user = document.getElementById("user-name").value
-    const password = document.getElementById("user-password").value
-
-    if (user == "gaaleo" && password == "1234") {
-        $("#user-name").val("");
-        $("#user-password").val("");
-    } else {
-        alert("Usuario y/o contraseña incorrecta")
-        $("#user-password").val("");
+function printUserTable(data) {
+    let html = '';
+    for (let i = 0; i < data.length; i++) {
+        html += `<tr data-user-id=${data[i].user_id}>\n\
+                    <td>${data[i].user}</td>
+                    <td>${data[i].name}</td>
+                    <td>${data[i].lastname}</td>
+                    <td>${data[i].email}</td>
+                    <td>
+                    <button class='btn btn-sm btn-info pull-left check' data-user-id="${data[i].user_id}" onclick="showModal('editUser',${data[i].user_id})>Editar</button>    
+                    <button class='btn btn-sm btn-danger pull-left check' data-user-id="${data[i].user_id}" onclick="showModal('deleteUser',${data[i].user_id})>Eliminar</button>
+                    </td>
+                </tr>`;
     }
-}
+    $("#user-table-body").html(html);
+};
+
+function saveUser(operator) {
+
+    const user = $("#user").val();
+    const name = $("#name").val();
+    const lastname = $("#lastname").val();
+    const email = $("#email").val();
+    const password = $("#password").val();
+    const userId = $("#user-id").val();
+
+    ipcRenderer.send("db-" + operator, {
+        table: "users",
+        data: {
+            user,
+            name,
+            lastname,
+            email,
+            password,
+            id: userId
+        },
+        purpose: operator + "User"
+    });
+
+};
+
+ipcRenderer.on("insertUser", (event, status) => {
+    if (status.success) {
+        $("#user-table-body").append(`<tr data-user-id=${status.data.user_id}>\n\
+        <td>${(status.data.user)}</td>
+        <td>${firstLetterToUpperCase(status.data.name)}</td>
+        <td>${firstLetterToUpperCase(status.data.lastname)}</td>
+        <td>${status.data.email}</td>
+        <td>
+        <button class='btn btn-sm btn-info pull-left check' data-user-id="${status.data.user_id}" onclick="showModal('editUser',${status.data.user_id})">Editar</button>
+        <button class='btn btn-sm btn-danger pull-left check' data-user-id="${status.data.user_id}" onclick="showModal('deleteUser',${status.data.user_id})">Eliminar</button>
+        </td>
+    </tr>`)
+
+        $("#user").val('');
+        $("#name").val('');
+        $("#last-name").val('');
+        $("#email").val('');
+        $("#password").val('');
+
+        fillAlert("¡Carga  del usuario exitosa!", "success", "user");
+
+    } else {
+        fillAlert("Error en la carga del usuario", "danger", "user");
+        console.log(status.err);
+    }
+});
+
+ipcRenderer.on("updateUser", (event, status) => {
+    if (status.success) {
+
+        const children = document.querySelector(`tr[data-user-id="${$("#user-id").val()}"]`).children
+    
+        children.item(0).innerHTML = $("#user").val();
+        children.item(1).innerHTML = $("#name").val();
+        children.item(2).innerHTML = $("#lastname").val();
+        children.item(3).innerHTML = $("#email").val();
+
+        fillAlert("¡Edición  del usuario exitosa!", "success", "user");
+
+        setTimeout(function () { $("#user-modal").modal("hide"); }, 1000);
+        
+    } else {
+        fillAlert("Error en la edición del usuario", "danger", "user");
+        console.log(status.err);
+    }
+});
+
+function deleteUser() {
+
+    const userId = $("#user-id").val();
+
+    ipcRenderer.send("db-delete", { table: "users", data: { id: userId }, purpose: "deleteUser" });
+
+};
+
+ipcRenderer.on("deleteUser", (event, status) => {
+    if (status.success) {
+
+        fillAlert("¡Eliminación del usuario existosa!", "success", "user");
+
+        setTimeout(function () { $("#user-modal").modal("hide"); }, 1000);
+
+    } else {
+
+        fillAlert("Error en la eliminación del usuario", "danger", "user");
+
+        console.log(status.err);
+    }
+});
+
+//function checkUser() {
+//    const user = document.getElementById("user-name").value
+//    const password = document.getElementById("user-password").value
+//
+//    if (user == "gaaleo" && password == "1234") {
+//        $("#user-name").val("");
+//        $("#user-password").val("");
+//    } else {
+//        alert("Usuario y/o contraseña incorrecta")
+//        $("#user-password").val("");
+//    }
+//}
